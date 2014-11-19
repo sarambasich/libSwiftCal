1efacc05e3f768ab0850f50004542efb8483d6fe
Your Correct Name <AritsOracle@gmail.com>
2014-11-18 21:58:55 -0500

    Initial Commit

---
+++ libSwiftCal/libSwiftCal.h
@@ -0,0 +1,19 @@
+//
+//  libSwiftCal.h
+//  libSwiftCal
+//
+//  Created by Stefan Arambasich on 11/18/14.
+//  Copyright (c) 2014 Stefan Arambasich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libSwiftCal.
+FOUNDATION_EXPORT double libSwiftCalVersionNumber;
+
+//! Project version string for libSwiftCal.
+FOUNDATION_EXPORT const unsigned char libSwiftCalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libSwiftCal/PublicHeader.h>
+
+
