dbae240eea447754fcc82cf1e129855fb72be238
Stefan Arambasich <ArtisOracle@gmail.com>
2014-12-01 23:58:36 -0500

    Legal stuffs.

--- libSwiftCalTests/libSwiftCalTests.swift
+++ libSwiftCalTests/libSwiftCalTests.swift
@@ -3,8 +3,26 @@
 //  libSwiftCalTests
 //
 //  Created by Stefan Arambasich on 11/10/14.
+//  
 //  Copyright (c) 2014 Stefan Arambasich. All rights reserved.
-//
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import UIKit
 import XCTest
@@ -32,7 +50,7 @@ class libSwiftCalTests: XCTestCase {
             let c = { (cal: Calendar) -> Void in
                 calendar = cal
                 
-                XCTAssert(cal.prodID.stringValue! == "-/MyList App/0.1a/EN", "Unexpected prodID")
+                XCTAssert(cal.prodID.stringValue! == "-/libSwiftCal/0.1a/EN", "Unexpected prodID")
                 XCTAssert(cal.version.stringValue! == "2.0", "Unexpected version")
                 XCTAssert(cal.calscale.stringValue! == "GREGORIAN", "Unexpected calscale")
                 
@@ -93,7 +111,7 @@ class libSwiftCalTests: XCTestCase {
             let json: [String : AnyObject] = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &err)! as [String : AnyObject]
             let cal = Calendar(dictionary: json)
             
-            XCTAssert(cal.prodID.stringValue! == "-/MyList App/0.1a/EN", "Unexpected prodID")
+            XCTAssert(cal.prodID.stringValue! == "-/libSwiftCal/0.1a/EN", "Unexpected prodID")
             XCTAssert(cal.version.stringValue! == "2.0", "Unexpected version")
             XCTAssert(cal.calscale.stringValue! == "GREGORIAN", "Unexpected calscale")
             
